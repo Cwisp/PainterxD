5c83bb9359a1ec8fcd7939f7e8a28e222447875d
Eric Wang <eric.zhexuan.wang@gmail.com>
2018-11-17 12:28:32 -0800

    asdf

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,4 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-import java.awt.Graphics;
 
 public class Main {
 
@@ -22,58 +15,28 @@ public class Main {
 	public static void main(String[] args) {
 		Window window = new Window(width, height);
 		
-		JPanel panel = new JPanel();
-		
-		panel.setLayout(null);
-		panel.setBounds(0, 0, window.getWidth(), window.getHeight());
-		window.add(panel);
-		BufferedImage myPicture = null;
-		try {
-			myPicture = ImageIO.read(new File("assets/trojanbox2.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-//		Graphics g = panel.getGraphics();
-//		int x = 0;
-//		int y = 0;
-//		long time = 0;
-//		while(true) {
-//			if (System.currentTimeMillis() - time >= 100) {
-//				g.clearRect(0, 0, panel.getWidth(), panel.getHeight());
-//				time = System.currentTimeMillis();
-//				g.drawImage(myPicture, x, y, null);
-//				x+=10;
-//				y+=10;
-//			}	
-//		}
-		
-		//Img img = new Img("assets/red200x400.png", 0, 0);
-		
 		map = new Map();
 		window.add(map);
-		long time = 0;
-		while (true) {
-			if (System.currentTimeMillis() - time >= 5) {
-				time = System.currentTimeMillis();
-				map.update();
-			}
-		}
-//		inGame = true;
-//		gameLoop();
+		
+		Player player = new Player();
+		
+		inGame = true;
+		gameLoop();
 	}
 	
 
 	public static void gameLoop() {
+		long time = System.currentTimeMillis();
 		while (inGame) {
-			map.update();
-			//update map: map will scroll down
-			//update enemies: projectiles will also scroll down 
-			//update player location to location in player class
-			//at set time intervals, will spawn another enemy
-			//check collisions
-			
+			if (System.currentTimeMillis() - time >= 5) {
+				time = System.currentTimeMillis();
+				map.update();
+				//update map: map will scroll down
+				//update enemies: projectiles will also scroll down 
+				//update player location to location in player class
+				//at set time intervals, will spawn another enemy
+				//check collisions
+			}
 		}
 	}
 }
