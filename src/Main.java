e60b6bab5409532adfb2736ba95ed8988496c1ba
Eric Wang <eric.zhexuan.wang@gmail.com>
2018-11-17 14:54:09 -0800

    added assets

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,31 @@ import java.util.ArrayList;
 public class Main {
 
 	public static ArrayList<Enemy> enemies = new ArrayList<Enemy>();
-	
+
 	public static boolean inGame = false;
-	
-	//screen dimensions
+
+	// screen dimensions
 	public static final int width = 400;
 	public static final int height = 600;
-	
+
 	public static Window window;
 	public static Map map;
 	public static Player p;
-		
+
 	public static void main(String[] args) {
 		window = new Window(width, height);
-		
+
 		map = new Map();
 		window.setMap(map);
-		
+
 		p = new Player();
 		window.setPlayer(p);
-		
+
 		window.setEnemies(enemies);
-		
+
 		inGame = true;
 		gameLoop();
 	}
-	
 
 	public static void gameLoop() {
 		long time = System.currentTimeMillis();
@@ -41,7 +40,7 @@ public class Main {
 			if (System.currentTimeMillis() - time >= 5) {
 				time = System.currentTimeMillis();
 				map.update();
-				for (int i = enemies.size()-1; i >= 0; i--) {
+				for (int i = enemies.size() - 1; i >= 0; i--) {
 					enemies.get(i).update();
 					if (enemies.get(i).getY() >= 600) {
 						enemies.remove(i);
@@ -49,7 +48,7 @@ public class Main {
 				}
 				if (p.checkCollisions(enemies))
 					inGame = false;
-				//check for collisions
+				// check for collisions
 				window.draw();
 			}
 		}
