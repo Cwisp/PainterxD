86e4fff84d37af541367bdbddcdb37bdeb432b86
Eric Wang <eric.zhexuan.wang@gmail.com>
2018-11-17 10:48:26 -0800

    created classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -7,14 +12,22 @@ public class Main {
 	public static boolean inGame = false;
 	
 	public static void main(String[] args) {
-		Window window = new Window(400,400);
+		Window window = new Window(600,600);
 		
 		JPanel panel = new JPanel();
 		
 		panel.setLayout(null);
 		panel.setBounds(0, 0, window.getWidth(), window.getHeight());
 		
-		JLabel img = new JLabel(new ImageIcon("trojanbox.png"));
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File("src/trojanbox.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(myPicture.getHeight());
+		JLabel img = new JLabel(new ImageIcon(myPicture));
 		
 		panel.add(img);
 		
